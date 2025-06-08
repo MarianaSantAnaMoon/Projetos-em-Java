8a0a653a03cf12c63a31873dc4957249e73ac8dd
Mariana <mariana.souza09@outlook.com>
2025-06-08 15:57:23 -0300

    Adicionado projeto Vetor_ForAprimorado

---
+++ Vetor_ForAprimorado/src/Principal_foraprimorado/Vetor_ForAprimorado.java
@@ -0,0 +1,16 @@
+package Principal_foraprimorado;
+
+
+public class Vetor_ForAprimorado {
+
+    
+    public static void main(String[] args) {
+        
+        String nomes[]={"Astrogildo Souza", "Don Quixote", "Patati Patata"};
+        
+        for(String nome : nomes){
+            System.out.println("Olá " + nome+ "!");
+        }
+    }
+    
+}
