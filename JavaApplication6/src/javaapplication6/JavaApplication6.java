f6fc7865adabc80f8a2865e385099912356652ec
Mariana <mariana.souza09@outlook.com>
2025-06-08 15:47:17 -0300

    Adicionado projeto JavaApplication6

---
+++ JavaApplication6/src/javaapplication6/JavaApplication6.java
@@ -0,0 +1,13 @@
+package javaapplication6;
+
+
+public class JavaApplication6 {
+
+    
+    public static void main(String[] args) {
+       for (int s = 1, d = 10; s <= 10 & d >= 1; s++, d--){
+        System.out.printf("%d \t %d\n", s,d);
+    }
+    }
+    
+}
