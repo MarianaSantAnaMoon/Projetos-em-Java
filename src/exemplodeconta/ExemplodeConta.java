492c8b422bb63f40e4318fd5be97f49d983ba03d
Mariana <mariana.souza09@outlook.com>
2025-06-08 14:28:43 -0300

    Primeiro commit do projeto Java

---
+++ src/exemplodeconta/ExemplodeConta.java
@@ -0,0 +1,14 @@
+package exemplodeconta;
+
+public class ExemplodeConta {
+  
+    public static void main(String[] args) {
+        int x = 2;
+        x ++;
+        x += 171 % 2;
+               
+                System.out.println("O valor de x é: " + x);
+//DEVE RETORNAR 4
+    }
+
+}
